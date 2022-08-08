6eb3c8bc27709a644bc66d3dd38cefac8e5a0cd3
Seba Rojo <srojoa19@gmail.com>
2022-08-08 15:21:43 -0400

    mini fix

--- components/utils/MenuContact.jsx
+++ components/utils/MenuContact.jsx
@@ -6,6 +6,7 @@ const MenuContact = () => {
           <a
               href='https://www.linkedin.com/in/seba-rojo/'
               target='_blank'
+              rel='noreferrer'
           >
               <div className='rounded-full bg-white p-3 md:p-3 cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-[#64ffda]'>
                   <FaLinkedinIn size={25} color='black'/>
@@ -14,6 +15,7 @@ const MenuContact = () => {
           <a
               href='https://github.com/ReditioN019'
               target='_blank'
+              rel='noreferrer'
           >
               <div className='rounded-full bg-white p-3 md:p-3  cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-[#64ffda]'>
                   <FaGithub size={25}  color='black'/>
