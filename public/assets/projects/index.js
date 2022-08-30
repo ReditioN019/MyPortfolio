c81f459f4b91f50882e042dcff81c9a51f213f05
Sebastián <srojoa19@gmail.com>
2022-08-30 12:53:32 -0400

    mini fix

--- public/assets/projects/index.js
+++ public/assets/projects/index.js
@@ -4,7 +4,7 @@
 import GiffApp from './gifApp.png';
 import GiffAppFondo from './gifApp-fondo.png';
 import MiniApp from './miniApp.png';
-import TasksApp from './TasksApp.png';
+import TasksApp from './tasksApp.png';
 
 
 export {
